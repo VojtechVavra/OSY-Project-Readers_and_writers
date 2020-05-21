83ad010a1620a02a6a7721ee3d80b993bdb55453
Lord Vooja, the Guardian <60519144+VojtechVavra@users.noreply.github.com>
2020-05-21 17:23:29 +0200

    Delete stringParser.h

--- shared/stringParser.h
+++
@@ -1,9 +0,0 @@
-#ifndef STRINGPARSER_H
-#define STRINGPARSER_H
-
-#include "messages.h"
-
-int stringParser(const char* line, Message& message);
-//int readline(int fd, char *buf, const unsigned int _len, int empty_buffer = 0);
-
-#endif
